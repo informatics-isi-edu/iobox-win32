7d2f3cd13f28a46800c29db1aa568dc3c3c47d0f
svoinea <serban@isi.edu>
2014-04-23 01:04:34 +0000

    Removed the stdin/stdout/stderr parameters from the subprocess.

--- src/service/client.py
+++ src/service/client.py
@@ -275,17 +275,17 @@ class ErmrestClient (object):
                 shutil.rmtree('%s%s%s' % (observer.tiff, os.sep, sha256sum))
             os.mkdir('%s%s%s' % (observer.tiff, os.sep, sha256sum))
             args = [observer.convertor, 'CL', '-i', file_from, '-t', '%s%s%s' % (observer.tiff, os.sep, sha256sum), '-b']
-            p = subprocess.Popen(args, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, creationflags=subprocess.CREATE_NEW_CONSOLE)
+            p = subprocess.Popen(args, creationflags=subprocess.CREATE_NEW_CONSOLE)
             serviceconfig.logger.debug('Started the convertor')
-            stdoutdata, stderrdata = p.communicate()
+            p.wait()
             returncode = p.returncode
             serviceconfig.logger.debug('Convertor ended: %d' % returncode)
-            if returncode == 0 or returncode == 255:
+            if returncode == 0:
                 f = self.getTiffFile('%s%s%s' % (observer.tiff, os.sep, sha256sum))
                 return f
             else:
-                serviceconfig.logger.error('got convert exception:\nreturncode: %d\nstdout: %s\nstderr: %s\n' % (returncode, stdoutdata, stderrdata))
-                self.sendMail('FAILURE TIFF', 'Exception generated during the TIFF conversion for the file "%s"\nreturncode: %d\nstdout: %s\nstderr: %s\n' % (file_from, returncode, stdoutdata, stderrdata))
+                serviceconfig.logger.error('got convert exception:\nreturncode: %d\n' % (returncode))
+                self.sendMail('FAILURE TIFF', 'Exception generated during the TIFF conversion for the file "%s"\nreturncode: %d\n' % (file_from, returncode))
                 return None
         except:
             et, ev, tb = sys.exc_info()
